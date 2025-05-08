4661193e8b206f05a296a3fc7e78d665337d3ab4
Zeki411 <nguyenhatrung411@gmail.com>
2025-05-08 17:32:17 +0000

    Add missing newline and reformat update.sh for clarity

--- update.sh
+++ update.sh
@@ -3,3 +3,6 @@ IMAGE_NAME="ghcr.io/serene4mr/mowbot_legacy:latest"
 
 # pull the docker image
 docker pull "$IMAGE_NAME"
+
+# copy common data directory to $HOME_DIR
+cp -r "$SCRIPT_DIR/mowbot_legacy_data" "$HOME_DIR/"
