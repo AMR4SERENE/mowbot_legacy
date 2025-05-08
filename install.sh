4e59ad66d3e94bc0b99add427b9a1e857202b11a
Zeki411 <nguyenhatrung411@gmail.com>
2025-05-08 14:07:52 +0000

    Remove SSH agent setup and clean up install script

--- install.sh
+++ install.sh
@@ -8,19 +8,11 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # run udev rules
 bash "$SCRIPT_DIR/udev/create_udev_rules.sh"
 
-# ensure ssh-agent is running
-eval "$(ssh-agent -s)"
-# add ssh key to agent
-ssh-add "$HOME_DIR/.ssh/id_rsa"
-
 # first, build or pull the docker image
 bash "$SCRIPT_DIR/docker/build.sh"
 
 # copy common data directory to $HOME_DIR
 cp -r "$SCRIPT_DIR/mowbot_legacy_data" "$HOME_DIR/"
 
-
-# remove src
-# rm -rf "$SCRIPT_DIR/src"
 
 
